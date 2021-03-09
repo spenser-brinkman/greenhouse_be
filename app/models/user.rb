28156e11eb59db4c932978e25b1e2599f50be823
spenser6131 <brinkman.spenser@gmail.com>
2021-03-09 14:01:01 -0800

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  
+  validates :username, uniqueness: true, presence: true
+  # validate :password_complexity
+  validates_confirmation_of :password
+  
+  has_secure_password
+
+  has_many :spaces
+  has_many :plants
+  
+  # def password_complexity
+  #   return if password.blank? || password =~ /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,30}$/
+  #   errors.add :password, "does not meet requirements."
+  # end
+
+end
