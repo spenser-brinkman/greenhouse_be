d6af7f3dfe8ef13a35145e1485520a71690682e4
spenser6131 <brinkman.spenser@gmail.com>
2021-03-10 12:36:54 -0800

    fix cors bug

--- config/application.rb
+++ config/application.rb
@@ -34,15 +34,5 @@ module Greenhouse2Backend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :patch, :options, :head]
-        )
-      end
-    end
   end
 end
